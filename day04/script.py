1bc4c4d892d7eb671fd795508956c0dc9af241ef
BuildTools <unconfigured@null.spigotmc.org>
2022-12-03 23:18:50 -0600

    day04 part 1

---
+++ day04/script.py
@@ -0,0 +1,16 @@
+
+with open("input", "r") as f: data = [x.strip() for x in f.readlines()]
+
+overlaps=0
+
+def parse(elf:str)->tuple[int,int]: return tuple(map(int,elf.split("-")))
+
+### Part 1 ###
+
+for line in data:
+    elf1, elf2 = line.split(",")
+    elf1x, elf1y = parse(elf1)
+    elf2x, elf2y = parse(elf2)
+    if set(range(elf1x,elf1y+1)).issubset(range(elf2x,elf2y+1)) or set(range(elf2x,elf2y+1)).issubset(range(elf1x,elf1y+1)): overlaps+=1
+
+print(f"### Part 1 ###\n{overlaps= }")
