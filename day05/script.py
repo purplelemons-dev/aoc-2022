494c04e6949a4e2ba5e622fe59141b3eb8ae5e76
Josh Smith <63922192+purplelemons-dev@users.noreply.github.com>
2022-12-05 10:28:00 -0600

    day05 part 1

---
+++ day05/script.py
@@ -0,0 +1,26 @@
+
+with open("input",'r') as f: data = [x for x in f.readlines()]
+
+### Part 1 ###
+
+initial = data[:8]
+instructions = [x.strip() for x in data[10:]]
+
+def parse_initial_cargo(initial:list[str])->dict[int,list[str]]:
+    cargo = {0: [], 1: [], 2: [], 3: [], 4: [], 5: [], 6: [], 7: [], 8: []}
+    for i in initial:
+        for j in cargo:
+            char = i[j*4+1]
+            if char!=" ": cargo[j].append(char)
+    for i in cargo: cargo[i] = cargo[i][::-1]
+    return cargo
+
+cargo = parse_initial_cargo(initial)
+for i in instructions:
+    move = int(i.split(" ")[1])
+    from_ = int(i.split(" ")[3])-1
+    to = int(i.split(" ")[5])-1
+    for j in range(move):
+        cargo[to].append(cargo[from_].pop())
+
+print(f"### Part 1 ###\nMessage: {''.join(i[-1] for i in cargo.values())}")
