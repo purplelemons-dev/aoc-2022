ec71184d784df0cc0f5a80ef6228181dc37ada06
BuildTools <unconfigured@null.spigotmc.org>
2022-12-11 16:44:06 -0600

    day08 part 2

--- day08/script.py
+++ day08/script.py
@@ -15,15 +15,11 @@ def transpose(array:list[list])->list[list]:
 array=[]
 key=[]
 for line in data: array.append([int(x) for x in line])
-#print(f"{len(array)= }")
-#print(f"{len(array[0])= }")
 transposed = transpose(array)
 
 for i,row in enumerate(array):
     current=[]
-    if i%100==0: print(f"{row= }")
     for j,cell in enumerate(row):
-        #print(f"{(i,j)= }")
         if 0<i<len(array)-1 and 0<j<len(row)-1:
             current.append(
                 all([
@@ -52,3 +48,42 @@ for i,row in enumerate(array):
 
 treesVisible = sum([sum(x) for x in key])
 print(f"### Part 1 ###\n{treesVisible= }")
+
+### Part 2 ###
+keys2=[]
+
+for i,row in enumerate(array):
+    subtotal=[]
+    for j,cell in enumerate(row):
+        if 0<i<len(array)-1 and 0<j<len(row)-1:
+            # \/ stupidest piece of code i've ever written \/
+            up,down,left,right = 0,0,0,0
+            # Left
+            current = row[:j][::-1]
+            for x in current:
+                left+=1
+                if x>=cell: break
+            # Right
+            current = row[j+1:]
+            for x in current:
+                right+=1
+                if x>=cell: break
+            # Up
+            current = transposed[j][:i][::-1]
+            for x in current:
+                up+=1
+                if x>=cell: break
+            # Down
+            current = transposed[j][i+1:]
+            for x in current:
+                down+=1
+                if x>=cell: break
+            subtotal.append(up*down*left*right)
+        else: continue
+    keys2.append(subtotal)
+
+total=[]
+for i in keys2:
+    # problems with empty lists for some reason dont ask me why
+    if i: total.append(max(i))
+print(f"### Part 2 ###\n{max(total)= }")
