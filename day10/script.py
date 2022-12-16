3eee6f4bc974894f106827069b78939849c08e77
Josh Smith <63922192+purplelemons-dev@users.noreply.github.com>
2022-12-16 11:59:19 -0600

    day 10 part 2 incomplete

--- day10/script.py
+++ day10/script.py
@@ -8,6 +8,8 @@ class CPU:
         self.clock = 0
         self.instructions = instructions
         self.registerSum = 0
+        self.spritePos = 0
+        self.litPixels = []
         self.run()
 
     @staticmethod
@@ -20,10 +22,24 @@ class CPU:
             timer = self.parse_instruction(inst)
             for j in range(timer):
                 self.clock += 1
+                # During cycle n
+                ### Pre-execution ###
                 if (self.clock + 20) % 40 ==0 and self.X:
                     self.registerSum += self.clock * self.X
+                if self.clock in range(self.spritePos%40,(self.spritePos+3)%40):
+                    
+                ### Execution ###
                 if j==timer-1 and inst.split(" ")[0] == "addx":
                     self.X += int(inst.split(" ")[-1])
 
+    @property
+    def render(self):
+        return f"Clock: {self.clock}"
+
+
 cpu = CPU(data)
-print(f"### Part 1 ###\nRegister sum: {cpu.registerSum}") # >7020
+print(f"### Part 1 ###\nRegister sum: {cpu.registerSum}\n{cpu.clock= }")
+
+
+### Part 2 ###
+
