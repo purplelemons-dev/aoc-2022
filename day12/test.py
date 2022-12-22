bc8de3840a6e8c3031a6f797e2501f16b0f30a43
Josh Smith <63922192+purplelemons-dev@users.noreply.github.com>
2022-12-22 13:00:03 -0600

    pathfinder.solved returns textmap of solved maze

--- day12/test.py
+++ day12/test.py
@@ -1,4 +1,13 @@
 
+given_map="""
+#S#######
+#   #  E#
+# # #   #
+# # #   #
+#       #
+#########"""[1:]
+
+
 class coord(complex):
     @property
     def x(self):
@@ -10,21 +19,15 @@ class coord(complex):
         return iter((self.x,self.y))
 
 
-given_map="""
-#S#######
-# # #  E#
-#       #
-# # #   #
-#       #
-#########"""[1:]
-
 class node:
     def __eq__(self, __o:'node') -> bool: return self.pos==__o.pos
     def __ne__(self, __o:'node') -> bool: return not self==__o
+    def __lt__(self, __o:'node') -> bool: return self.distance<__o.distance
     def __hash__(self) -> int: return hash(self.pos)
     def __init__(self,pos:coord,value:str):
         self.pos=pos
         self.value=value
+        self.parent:node=None
         self.distance=None
 
     @property
@@ -46,14 +49,16 @@ class node:
         temp=set()
         for neighbor in self.neighbors(map,dim):
             if neighbor.discovered:
-                temp.add(neighbor.distance)
+                temp.add(neighbor)
             else:
                 yield neighbor
-        self.distance=min(temp)+1 if temp else 0
+        self.parent = min(temp,key=lambda i:i.distance) if temp else None
+        self.distance=self.parent.distance+1 if self.parent else 0
 
 
 class pathfinder:
     def __init__(self,textmap:str):
+        self.textmap=textmap
         self.map:dict[coord,node]={
             coord(x,y):node(coord(x,y),cell)
                 for y,line in enumerate(textmap.splitlines())
@@ -72,6 +77,20 @@ class pathfinder:
             if self.end.discovered:
                 return self.end.distance
             self.current=next_nodes
+        
+    @property
+    def solved(self):
+        current=self.end
+        temp=[list(i) for i in self.textmap.splitlines()]
+        while 1:
+            if current.value=="S": return "\n".join("".join(i) for i in temp)
+            current=current.parent
+            pos_x, pos_y = current.pos
+            temp[pos_y][pos_x]="."
+
 
 if __name__=="__main__":
-    print(pathfinder(given_map).run())
+    path = pathfinder(given_map)
+    print(path.run())
+    print(path.solved)
+
